81f92db12a6ddaa135ba42bce2dab978e8892477
Nat <nat@cmmc.io>
2019-06-14 18:47:16 +0700

    add countdown and rocket.

--- functions/index.js
+++ functions/index.js
@@ -25,48 +25,10 @@ exports.line_nat_chatbot_webhook = functions.https.onRequest((req, res) => {
   }
 });
 
-exports.pps_rocket_bot = functions.https.onRequest((req, res) => {
-  let channelAccessToken = functions.config()['pps-rocket-bot']['line']['channel-access-token'];
-  let channelSecret = functions.config()['pps-rocket-bot']['line']['channel-secret'];
-  const client = new line.Client({channelAccessToken, channelSecret});
-
-  if (req.method === 'POST') {
-    const body = Object.assign(req.body);
-    body.events.map(event => {
-      if (event.type === 'message') {
-        if (event.message.type === 'text') {
-          if (textMapping[event.message.text]) {
-            let msg = textMapping[event.message.text].text ||
-                event.message.text;
-            publishMqtt({topic: 'cf/rocket', msg});
-          } else {
-            publishMqtt({topic: 'cf/rocket', msg: event.message.text});
-          }
-          console.log(`event=`, event);
-
-          client.replyMessage(event.replyToken,
-              constructReplyMessage(event.message.text)).then(r => {
-            //res.status(200).send('post ok');
-          });
-        } else {
-          client.replyMessage(event.replyToken,
-              constructReplyMessage(`ยังไม่รองรับข้อความประเภท ${event.message.type}`)).
-          then(r => {
-            //res.status(200).send('post ok');
-          });
-        }
-        res.status(200).send('post ok');
-      } else if (req.method === 'GET') {
-        res.status(200).send('rocket GET OK ' + JSON.stringify(req));
-      } else {
-        res.status(500).send('Forbidden!');
-      }
-    });
-  }
-});
-
+exports.pps_rocket_bot = functions.https.onRequest(require('./fn/rocket'));
 exports.pps_pants_bot = functions.https.onRequest(require('./fn/pants'));
 exports.pps_arrow_bot = functions.https.onRequest(require('./fn/arrow'));
+exports.pps_countdown_bot = functions.https.onRequest(require('./fn/countdown'));
 
 exports.line_cmmc_chatbot_webhook = functions.https.onRequest((
     req, res) => {
