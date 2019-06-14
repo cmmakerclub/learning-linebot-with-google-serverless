da8fde7588db6013eff9c030fd2b55ce67397dad
Nat <nat@cmmc.io>
2019-06-14 16:12:46 +0700

    update fn.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
-const functions = require("firebase-functions");
-const line = require("@line/bot-sdk");
-const admin = require("firebase-admin");
-const {get, post} = require("./utils");
-const {flex1} = require("./flex.messages");
+const functions = require('firebase-functions');
+const line = require('@line/bot-sdk');
+const admin = require('firebase-admin');
+const {get, post} = require('./utils');
+const {flex1} = require('./flex.messages');
 
 admin.initializeApp();
 
@@ -10,65 +10,130 @@ const httpEndpoint = functions.config().iot.http.endpoint;
 let topic = `CMMC/PLUG-002/$/command`;
 
 exports.line_nat_chatbot_webhook = functions.https.onRequest((req, res) => {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const body = Object.assign(req.body);
-    res.status(200).send("post ok");
-  } else if (req.method === "GET") {
+    res.status(200).send('post ok');
+  } else if (req.method === 'GET') {
     res.status(200).
-    send("line_nat_chatbot_webhook GET OK " + JSON.stringify(req));
+    send('line_nat_chatbot_webhook GET OK ' + JSON.stringify(req));
   } else {
-    res.status(500).send("Forbidden!");
+    res.status(500).send('Forbidden!');
   }
 });
 
 exports.line_KornWtp_chatbot_webhook = functions.https.onRequest((req, res) => {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const body = Object.assign(req.body);
-    res.status(200).send("post ok");
-  } else if (req.method === "GET") {
+    res.status(200).send('post ok');
+  } else if (req.method === 'GET') {
     res.status(200).
-    send("line_KornWtp_chatbot_webhook GET OK " + JSON.stringify(req));
+    send('line_KornWtp_chatbot_webhook GET OK ' + JSON.stringify(req));
   } else {
-    res.status(500).send("Forbidden!");
+    res.status(500).send('Forbidden!');
   }
 });
 
 exports.line_Boat_chatbot_webhook = functions.https.onRequest((req, res) => {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const body = Object.assign(req.body);
-    res.status(200).send("post ok");
-  } else if (req.method === "GET") {
+    res.status(200).send('post ok');
+  } else if (req.method === 'GET') {
     res.status(200).
-    send("line_Boat_chatbot_webhook GET OK " + JSON.stringify(req));
+    send('line_Boat_chatbot_webhook GET OK ' + JSON.stringify(req));
   } else {
-    res.status(500).send("Forbidden!");
+    res.status(500).send('Forbidden!');
   }
 });
 
-exports.line_cmmc_chatbot_webhook = functions.https.onRequest((req, res) => {
-  const config = {
-    channelAccessToken: functions.config().cmmc.line["channel-access-token"],
-    channelSecret: functions.config().cmmc.line["channel-secret"],
-  };
-  const client = new line.Client(config);
-  if (req.method === "POST") {
+exports.pps_rocket_bot = functions.https.onRequest((req, res) => {
+  let channelAccessToken = functions.config()['pps-rocket-bot']['line']['channel-access-token'];
+  let channelSecret = functions.config()['pps-rocket-bot']['line']['channel-secret'];
+  const client = new line.Client({channelAccessToken, channelSecret});
+
+  if (req.method === 'POST') {
     const body = Object.assign(req.body);
     body.events.map(event => {
-      if (event.type === "message" && event.message.type === "text") {
-        console.log("-----------------------------------------");
+      if (event.type === 'message' && event.message.type === 'text') {
+        let data = constructReplyMessage(event.message.text);
+        client.replyMessage(event.replyToken, data).then(res => { });
+        res.status(200).send('post ok');
+      } else if (req.method === 'GET') {
+        res.status(200).send('rocket GET OK ' + JSON.stringify(req));
+      } else {
+        res.status(500).send('Forbidden!');
+      }
+    });
+  }
+});
+
+const constructReplyMessage = (text) => {
+  let data = {type: 'text', text};
+  return data;
+};
+
+exports.pps_arrow_bot = functions.https.onRequest((req, res) => {
+  let channelAccessToken = functions.config()['pps-arrow-bot']['line']['channel-access-token'];
+  let channelSecret = functions.config()['pps-arrow-bot']['line']['channel-secret'];
+  const client = new line.Client({channelAccessToken, channelSecret});
+
+  if (req.method === 'POST') {
+    const body = Object.assign(req.body);
+    body.events.map(event => {
+      if (event.type === 'message' && event.message.type === 'text') {
+        let data = constructReplyMessage(event.message.text);
+        client.replyMessage(event.replyToken, data).then(res => { });
+        res.status(200).send('post ok');
+      } else if (req.method === 'GET') {
+        res.status(200).
+        send('pps_arrow_bot GET OK ' + JSON.stringify(req));
+      } else {
+        res.status(500).send('Forbidden!');
+      }
+    });
+  }
+});
+
+exports.line_Boat_chatbot_webhook = functions.https.onRequest((
+    req, res) => {
+  if (req.method === 'POST') {
+    const body = Object.assign(req.body);
+    body.events.map(event => {
+      if (event.type === 'message' && event.message.type === 'text') {
+        client.replyMessage(event.replyToken, data).then(res => {
+          console.log(`reply result = `, res);
+        });
+      }
+    });
+
+    res.status(200).send('post ok');
+  } else if (req.method === 'GET') {
+    res.status(200).
+    send('line_Boat_chatbot_webhook GET OK ' + JSON.stringify(req));
+  } else {
+    res.status(500).send('Forbidden!');
+  }
+});
+
+exports.line_cmmc_chatbot_webhook = functions.https.onRequest((
+    req, res) => {
+  const config = {
+    channelAccessToken: functions.config().cmmc.line['channel-access-token'],
+    channelSecret: functions.config().cmmc.line['channel-secret'],
+  };
+  const client = new line.Client(config);
+  if (req.method === 'POST') {
+    const body = Object.assign(req.body);
+    body.events.map(event => {
+      if (event.type === 'message' && event.message.type === 'text') {
+        console.log('-----------------------------------------');
         console.log(`source type = ${event.source.type}`);
         console.log(`message text = ${event.message.text}`);
         console.log(`replyToken = ${event.replyToken}`);
         console.log(JSON.stringify(event));
 
-        let data = {
-          type: "text",
-          text: event.message.text,
-        };
-
-        if (event.message.text === "Nat") {
-          data.text = "หวัดดี";
-        } else if (event.message.text === "flex") {
+        if (event.message.text === 'Nat') {
+          data.text = 'หวัดดี';
+        } else if (event.message.text === 'flex') {
           data = flex1;
         } else {
           data.text = event.message.text;
@@ -80,13 +145,13 @@ exports.line_cmmc_chatbot_webhook = functions.https.onRequest((req, res) => {
 
         // calling mqtt bridge
         get(`${httpEndpoint}?topic=${topic}&command=${event.message.text}`);
-        console.log("/-----------------------------------------");
+        console.log('/-----------------------------------------');
       }
     });
-    res.status(200).send("post ok");
-  } else if (req.method === "GET") {
-    res.status(200).send("GET OK " + JSON.stringify(req));
+    res.status(200).send('post ok');
+  } else if (req.method === 'GET') {
+    res.status(200).send('GET OK ' + JSON.stringify(req));
   } else {
-    res.status(500).send("Forbidden!");
+
   }
 });
