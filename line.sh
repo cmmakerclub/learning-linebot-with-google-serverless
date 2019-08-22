0eba59737ad2ac084ac7483ee9d555c9d43c24b7
Nat <nat.wrw@gmail.com>
2019-08-22 23:25:06 +0700

    update

--- line.sh
+++ line.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST -H "Authorization: Bearer $LINE_TOKEN" -F "message=Cloud Functions DEPLOYED!" https://notify-api.line.me/api/notify
+curl -X POST -H "Authorization: Bearer $LINE_TOKEN" -F "message=Cloud Functions DEPLOYED! status=$?" https://notify-api.line.me/api/notify
