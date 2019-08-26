44493276460956216027304ab4ecf2a1726a1b41
Nat <nat.wrw@gmail.com>
2019-08-26 13:31:59 +0700

    update

--- line.sh
+++ line.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # curl -X POST -H "Authorization: Bearer $LINE_TOKEN" -F "message=Cloud Functions DEPLOYED! status=$?" https://notify-api.line.me/api/notify
-curl -X POST "$NOTIFY_URL"
+curl -X POST "$LINE_NOTIFY&message=dsc1 firebase functions DEPLOYED with status=$?"
